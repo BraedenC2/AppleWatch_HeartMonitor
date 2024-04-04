9876cdf835031a10151d04918205de3ec7913289
Braeden Connors <Braeden.c.connors@gmail.com>
2024-04-04 17:38:56 -0500

    Initial Commit

---
+++ WatchScreen Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchScreen Watch App
+//
+//  Created by Connors, Braeden C on 4/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchScreen Watch App/WatchScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchScreenApp.swift
+//  WatchScreen Watch App
+//
+//  Created by Connors, Braeden C on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchScreen_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
