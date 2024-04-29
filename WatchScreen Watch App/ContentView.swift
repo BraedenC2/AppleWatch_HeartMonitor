46a7a0de5038ed1359880d4f210df4d45ddceef6
BraedenC2 <Braeden.c.connors@gmail.com>
2024-04-29 15:39:28 -0500

    Fixed the deprecation issue (I think).

--- WatchScreen Watch App/ContentView.swift
+++ WatchScreen Watch App/ContentView.swift
@@ -1,24 +1,25 @@
-//
-//  ContentView.swift
-//  WatchScreen Watch App
-//
-//  Created by Connors, Braeden C on 4/4/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isOn = false
+    @ObservedObject private var healthKitManager = HealthWatcher()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Toggle("Heart Rate", isOn: $isOn)
+                .onChange(of: isOn) { newValue, newValue2 in
+                    if newValue {
+                        healthKitManager.requestAuthorization()
+                        healthKitManager.startHeartRateQuery(quantityTypeIdentifier: .heartRate)
+                    } else {
+                        healthKitManager.stopHeartRateQuery()
+                    }
+                }
+            
+            if isOn {
+                Text("Heart Rate: \(healthKitManager.heartRate, specifier: "%.0f") BPM")
+            }
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
--- WatchScreen Watch App/HealthWatcher.swift
+++ WatchScreen Watch App/HealthWatcher.swift
@@ -1,8 +1,40 @@
-//
-//  HealthWatcher.swift
-//  WatchScreen Watch App
-//
-//  Created by Connors, Braeden C on 4/4/24.
-//
-
 import Foundation
+import HealthKit
+
+class HealthWatcher: NSObject, ObservableObject {
+    private var healthStore: HKHealthStore?
+    private var heartRateQuery: HKQuery?
+    @Published var heartRate: Double = 0
+
+    override init() {
+        super.init()
+        healthStore = HKHealthStore()
+    }
+
+    func requestAuthorization() {
+        guard let heartRateType = HKObjectType.quantityType(forIdentifier: .heartRate) else {return}
+        let healthKitTypesToRead: Set<HKObjectType> = [heartRateType]
+        healthStore?.requestAuthorization(toShare: [], read: healthKitTypesToRead) { _, _ in }
+    }
+    func startHeartRateQuery(quantityTypeIdentifier: HKQuantityTypeIdentifier) {
+        let devicePredicate = HKQuery.predicateForObjects(from: [HKDevice.local()])
+        let query = HKAnchoredObjectQuery(
+            type: HKObjectType.quantityType(forIdentifier: quantityTypeIdentifier)!,
+            predicate: devicePredicate,
+            anchor: nil,
+            limit: HKObjectQueryNoLimit
+        ) { _, samples, _, _, _ in
+            guard let samples = samples as? [HKQuantitySample] else { return }
+            self.heartRate = samples.last?.quantity.doubleValue(for: HKUnit(from: "count/min")) ?? 0
+        }
+        healthStore?.execute(query)
+        self.heartRateQuery = query
+    }
+    
+    func stopHeartRateQuery() {
+            if let query = heartRateQuery {
+                healthStore?.stop(query)
+            }
+        }
+    
+}
